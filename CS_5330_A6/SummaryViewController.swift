a61d3771746b7be391ac3756963c65b5e56ccc49
Kyle Zimmerman <kzdesignsios@gmail.com>
2024-02-12 17:46:42 -0800

    added SummaryViewController and segue

---
+++ CS_5330_A6/SummaryViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SummaryViewController.swift
+//  CS_5330_A6
+//
+//  Created by Kyle Zimmerman on 2/12/24.
+//
+
+import UIKit
+
+class SummaryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- CS_5330_A6/ViewController.swift
+++ CS_5330_A6/ViewController.swift
@@ -14,6 +14,10 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func onCalculateButtonPress(_ sender: UIButton) {
+        self.performSegue(withIdentifier: "toSummaryView", sender: self)
+        
+    }
+    
 }
 
